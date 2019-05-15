7d9837a41293d50ea70abb7baae6424629b7d887
Paul Standley <paulstandley72@outlook.com>
2019-05-15 17:46:21 +0100

    Calculator

--- index.php
+++ index.php
@@ -8,38 +8,41 @@
     <title>PHP</title>
 </head>
 <body>
-    <?php
-        $hs = "<h1 style='color: blue'>";
-        $he = "</h1>";
-        $name = "Paul Standley";
-        $doing = " is learning <strong style='color: pink'>PHP</strong>";
-        echo $hs.$name.$doing.$he; 
-    ?>
+    <h1>PHP Calculator</h1>
     <hr>
-    <form method="GET">
-        <input type="text" name="answer">
-        <button>Submit</button>
+    <form>
+        <input type="text" name="num1" placeholder="Number 1">
+        <input type="text" name="num2" placeholder="Number 2">
+        <select name="operator">
+            <option>None</option>
+            <option>Add</option>
+            <option>Subtract</option>
+            <option>Multiply</option>
+            <option>Divide</option>
+        </select>
+        <br>
+        <button type="submit" name="submit" value="submit">Calculate</button>
     </form>
-    <?php
-    // switch stament
-    $x = $_GET['answer'];
-    $Theansweris = "The answer is ";
-    // runs without a value throws exseption
-    //$x = "a number";
-    switch($x) {
-        case 1: echo $Theansweris.$x;
-        break;
-        case 2: echo $Theansweris.$x;
-        break;
-        case 3: echo $Theansweris.$x;
-        break;
-        case 4: echo $Theansweris.$x;
-        break;
-        default: echo $Theansweris.$x;
-    }
-    echo "<hr>";
+    <p>The answer is : </p>
+    <?php 
+        if(isset($_GET['submit'])) {
+            $result1 = $_GET['num1'];
+            $result2 = $_GET['num2'];
+            $operator = $_GET['operator'];
+            switch($operator) {
+                case "None": echo "You need to select a opreater";
+                break;
+                case "Add": echo $result1+$result2;
+                break;
+                case "Subtract": echo $result1-$result2;
+                break;
+                case "Multiply": echo $result1*$result2;
+                break;
+                case "Divide": echo $result1/$result2;
+                break;
+            }
+        }
     ?>
-    <hr>
-    <script src="app.js"></script>
+    <script src="js/app.js"></script>
 </body>
 </html>
---
+++ index3.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+        $hs = "<h1 style='color: blue'>";
+        $he = "</h1>";
+        $name = "Paul Standley";
+        $doing = " is learning <strong style='color: pink'>PHP</strong>";
+        echo $hs.$name.$doing.$he; 
+    ?>
+    <hr>
+    <form method="GET">
+        <input type="text" name="answer">
+        <button>Submit</button>
+    </form>
+    <?php
+        // switch stament
+        $x = $_GET['answer'];
+        $Theansweris = "The answer is ";
+        // runs without a value throws exseption
+        //$x = "a number";
+        switch($x) {
+            case 1: echo $Theansweris.$x;
+            break;
+            case 2: echo $Theansweris.$x;
+            break;
+            case 3: echo $Theansweris.$x;
+            break;
+            case 4: echo $Theansweris.$x;
+            break;
+            default: echo $Theansweris.$x;
+        }
+    ?>
+    <hr>
+    <script src="js/app.js"></script>
+</body>
+</html>
